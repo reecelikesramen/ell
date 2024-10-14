63d434f274dd2f7559840dc7ea1972808268ec51
Reece Holmdahl <rholmdahl@gmail.com>
2024-10-13 20:53:39 -0500

    Spelling and misc improvements to instructor_ex.py

--- examples/providers/instructor_ex.py
+++ examples/providers/instructor_ex.py
@@ -20,15 +20,16 @@ client = instructor.from_openai(OpenAI())
 
 class InstructorProvider(OpenAIProvider):
     def translate_to_provider(self, *args, **kwargs):
-        """ This translates ell call param,eters to the provider call parameters.  IN this case instructor is jsut an openai client. 
-        so we can use the openai provider to do the translation. We just need to modify a few parameters because instructor doesn't support streaming."""
-        api_params= super().translate_to_provider(*args, **kwargs)
+        """ This translates ell call parameters to the provider call parameters.  In this case, instructor is just an OpenAI client. 
+        so we can use the OpenAI provider to do the translation. We just need to modify a few parameters because instructor doesn't support streaming."""
+        api_params = super().translate_to_provider(*args, **kwargs)
         # Streaming is not allowed by instructor.
         api_params.pop("stream", None)
         api_params.pop("stream_options", None)
         return api_params
     
-    def translate_from_provider(self,provider_response,
+    def translate_from_provider(self,
+            provider_response,
             ell_call : EllCallParams,
             provider_call_params : Dict[str, Any],
             origin_id : str, 
@@ -36,22 +37,27 @@ class InstructorProvider(OpenAIProvider):
         """This translates the provider response (the result of calling client.chat.completions.create with the parameters from translate_to_provider)
           to an an ell message. In this case instructor just returns a pydantic type which we can use to create an ell response model. """
         instructor_response = cast(BaseModel, provider_response) # This just means that the type is a pydantic BaseModel. 
-        if logger: logger(instructor_response.model_dump_json()) # Don't forget to log for verbose mode!
+        if logger:
+            # Don't forget to log for verbose mode!
+            logger(instructor_response.model_dump_json())
         return Message(role="assistant", content=ContentBlock(parsed=instructor_response)), {}
 
 # We then register the provider with ell. We will use InstructorProvider any time an instructor.Instructor type client is used.
 ell.register_provider(InstructorProvider(), instructor.Instructor)
 
 class UserDetail(BaseModel):
-    name: str
+    first_name: str
+    last_name: Optional[str]
     age: int
 
 
-@ell.complex(model="gpt-4-turbo-preview", client=client, response_model=UserDetail)
-def extract_user(details : str):
+@ell.complex(model="gpt-4o-mini", client=client, response_model=UserDetail)
+def extract_user(details : str) -> UserDetail:
     return f"Extract {details}"
 
-print(extract_user("Jason is 25 years old"))
 ell.init(verbose=True)
+message = extract_user("John Doe is 25 years old")
+user_details = message.parsed
+print(user_details)
 
 
